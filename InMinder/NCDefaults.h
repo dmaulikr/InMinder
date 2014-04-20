b55b9c33d3c07b034d3c706b04d053e6cc9739e4
nevercry <ysfnevercry@gmail.com>
2014-04-20 13:41:31 +0800

    change Code sign

---
+++ InMinder/NCDefaults.h
@@ -0,0 +1,13 @@
+//
+//  NCDefaults.h
+//  InMinder
+//
+//  Created by nevercry on 4/20/14.
+//  Copyright (c) 2014 nevercry. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NCDefaults : NSObject
+
+@end
---
+++ InMinder/NCDefaults.m
@@ -0,0 +1,13 @@
+//
+//  NCDefaults.m
+//  InMinder
+//
+//  Created by nevercry on 4/20/14.
+//  Copyright (c) 2014 nevercry. All rights reserved.
+//
+
+#import "NCDefaults.h"
+
+@implementation NCDefaults
+
+@end
